cb9ba79e83f7ce3dc40a2bd0d35e257e72f5d577
Sepehr <62986692+IIIphr@users.noreply.github.com>
2020-09-02 16:13:09 +0430

    a little jquery script added to file, exciting !

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
 	<head>
 		<title>My temp title :)</title>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+		<script>
+			$(document).ready(function(){
+				$("p").click(function(){
+					$(this).hide();
+				});
+			});
+		</script>
 	</head>
 
 	<body>
