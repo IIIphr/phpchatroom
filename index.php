f9623bc503f5d5b201a7ac62ee4e3f737f186217
Sepehr <62986692+IIIphr@users.noreply.github.com>
2020-09-02 14:37:46 +0430

    connected to mysql :)

--- index.php
+++ index.php
@@ -6,6 +6,18 @@
 	<body>
 		<p>My temp body :)</p>
 
-		<?php echo 'Hello !'; ?>
+		<?php echo '<p>Hello !</p>'; ?>
+
+		<?php
+
+		$mysql = new mysqli("localhost", "root", "", "php_chatroom");
+		if($mysql === false){
+			die("ERROR: Could not connect. " . $mysql->connect_error);
+		}
+		 
+		// Print host information
+		echo "Connect Successfully. Host info: " . $mysql->host_info;
+
+		?>
 	</body>
 </html>
