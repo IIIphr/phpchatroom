bd6d76ffe7650cf6af70b4a2d752ae29df68cf33
Sepehr <62986692+IIIphr@users.noreply.github.com>
2020-09-07 16:47:57 +0430

    checkpoint , adding jquery and auth

--- get_messages.php
+++ get_messages.php
@@ -3,8 +3,8 @@ $mysql = new mysqli("localhost", "root","", "php_chatroom");
 $req="SELECT * FROM messages";
 $res=$mysql->query($req);
 if ($res->num_rows > 0) {
-    while($row = $res->fetch_assoc()) {
-      echo $row['sender']." : ". $row['message'] . "   : " . $row['time'] ."<br>";
-    }
+  while($row = $res->fetch_assoc()) {
+    echo $row['sender']." : ". $row['message'] . "   : " . $row['time'] ."<br>";
   }
+}
 ?>
--- index.php
+++ index.php
@@ -1,17 +1,55 @@
 <html>
 	<head>
 		<title>Wlecome !</title>
+		<script src="jquery-3.5.1.js"></script>
 	</head>
 
 	<body>
 
-		<form action="messaging.php" method="POST" id="form">
-			user name :
-			<input type="text" id="rd" name="user">
-			<br>
-			<input type="submit" value="Submit">
+		<form action="" method="POST" id="loginform">
+			username : <input type="text" name="username" id="username"><br>
+			password : <input type="password" name="password" id="password"><br>
+			<input type="submit" value="login"><br>
 		</form>
 
+		<p id="error"></p>
+
+		<a href="register.php">No account ? register here</a>
+
 	</body>
+
+	<script>
+		$(function(){
+			$("#loginform").submit(function(){
+				$.post("logincheck.php",
+				{
+					username: document.getElementById("username").value,
+					password: document.getElementById("password").value
+				},
+				function(data,status){
+					if(data=="OK"){
+						form=document.createElement('form');
+						form.method="POST";
+						form.action="messaging.php";
+						userfield=document.createElement('input');
+						userfield.type = 'hidden';
+						userfield.name = 'username';
+						userfield.value = document.getElementById("username").value;
+						form.appendChild(userfield);
+						passfield=document.createElement('input');
+						passfield.type = 'hidden';
+						passfield.name = 'password';
+						passfield.value = document.getElementById("password").value;
+						form.appendChild(passfield);
+						document.body.appendChild(form);
+  						form.submit();
+					}
+					else{
+						document.getElementById("error").innerHTML="Can't login";
+					}
+				})
+			})
+		})
+	</script>
 
 </html>
