33c42b000b123619fc157761c5fe9f7ce1b86f98
Sepehr <62986692+IIIphr@users.noreply.github.com>
2020-09-09 21:37:33 +0430

    it's kinda realtime now ...

--- messaging.php
+++ messaging.php
@@ -49,5 +49,21 @@
 		}
 	</script>
 
+	<script>
+		setInterval(function(){
+			xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function() {
+				if (this.readyState == 4 && this.status == 200) {
+					var res="refreshed ! <br>";
+					document.getElementById("status").innerHTML = res;
+					document.getElementById("messages").innerHTML = this.responseText;
+				}
+			};
+			xhttp.open("POST", "get_messages.php", true);
+			xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			xhttp.send();
+		},1000);
+	</script>
+
 
 </html>
