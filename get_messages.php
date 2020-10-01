bb3f69016d8b0d5f7ba81824dc5f777346cc5982
Sepehr <62986692+IIIphr@users.noreply.github.com>
2020-10-01 19:10:50 +0330

    still desgining :) messaging now looks better :)

--- get_messages.php
+++ get_messages.php
@@ -1,10 +1,18 @@
 <?php
+session_start();
 $mysql = new mysqli("localhost", "root","", "php_chatroom");
 $req="SELECT * FROM messages";
 $res=$mysql->query($req);
 if ($res->num_rows > 0) {
   while($row = $res->fetch_assoc()) {
-    $msg="<div class=\"msg\" ><div class=\"sender\" >".$row['sender']."</div><div class=\"txt\" >".$row['message']."</div><div class=\"time\" >".$row['time']."</div></div>";
+    $msg="";
+    if($row['sender']==$_SESSION['sender']){
+      $msg=$msg . "<div class=\"messageself\"><div class=\"msgself";
+    }
+    else{
+      $msg=$msg . "<div class=\"messageother\"><div class=\"msg";
+    }
+    $msg=$msg . "\" ><div class=\"sender\" >".$row['sender']."</div><br><div class=\"txt\" >".$row['message']."</div><br><div class=\"time\" >".$row['time']."</div></div></div>";
     echo $msg;
   }
 }
